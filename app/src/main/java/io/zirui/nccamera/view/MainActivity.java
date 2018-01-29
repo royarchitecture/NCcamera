7af51130c85f79e19520fdbcf13a45f963d1d59e
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2018-01-29 12:28:58 -0800

    fix compatibility problem that cannot run on sdk.23

--- app/src/main/java/io/zirui/nccamera/view/MainActivity.java
+++ app/src/main/java/io/zirui/nccamera/view/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.zirui.nccamera.R;
